2750a25fc6e77e8a9918fabba2250ccdd1e68faa
Tuncay Çakıroğlu <Tuncay.cakiroglu@icloud.com>
2023-10-09 18:15:40 +0300

    Cari Ekle Sil Güncelle İşlemleri Tamamlandı.

--- EasyHesapOnlineTicariOtomasyon/Controllers/CariController.cs
+++ EasyHesapOnlineTicariOtomasyon/Controllers/CariController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using EasyHesapOnlineTicariOtomasyon.Models.Classes;
 
@@ -13,7 +14,7 @@ namespace EasyHesapOnlineTicariOtomasyon.Controllers
         Context c=new Context();    
         public ActionResult Index()
         {
-            var degerler = c.Carilers.ToList();
+            var degerler = c.Carilers.Where(x=> x.Durum==true).ToList();
             return View(degerler);
         }
 
@@ -25,9 +26,47 @@ namespace EasyHesapOnlineTicariOtomasyon.Controllers
         [HttpPost]
         public ActionResult YeniCari(Cariler p)
         {
+            p.Durum = true;
             c.Carilers.Add(p);
             c.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public ActionResult CariSil(int id)
+        {
+            var cr = c.Carilers.Find(id);
+            cr.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CariGetir(int id)
+        {
+            var cr = c.Carilers.Find(id);
+            return View("CariGetir", cr);
+        }
+        
+        public ActionResult CariGuncelle(Cariler p)
+        {
+            if (!ModelState.IsValid) 
+            {
+              return View("CariGetir");
+            }
+            var cari = c.Carilers.Find(p.Cariid);
+            cari.CariAd=p.CariAd;
+            cari.CariSoyad=p.CariSoyad;
+            cari.CariUnvan=p.CariUnvan;
+            cari.CariSehir=p.CariSehir;
+            cari.CariMail=p.CariMail;
+            cari.CariTelefon=p.CariTelefon;
+            cari.CariAdres=p.CariAdres;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MusteriSatis(int id)
+        {
+            var deger=c.SatisHarekets.Where(x=> x.Cariid==id).ToList();
+            var dgr = c.Carilers.Where(x => x.Cariid == id).Select(y => y.CariAd + " " + y.CariSoyad).FirstOrDefault();
+            ViewBag.dgr= dgr;
+            return View(deger);
         }
     }
 }
--- EasyHesapOnlineTicariOtomasyon/Models/Classes/Cariler.cs
+++ EasyHesapOnlineTicariOtomasyon/Models/Classes/Cariler.cs
@@ -13,32 +13,35 @@ namespace EasyHesapOnlineTicariOtomasyon.Models.Classes
         public int Cariid { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(50)]
+        [StringLength(50,ErrorMessage ="En Fazla 50 Karakter Girilebilir ! Lütfen Düzeltiniz!")]
         public string CariAd { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "En Fazla 50 Karakter Girilebilir ! Lütfen Düzeltiniz!")]
+        [Required(ErrorMessage ="Bu Alanı Boş Geçemezsiniz!")]
         public string CariSoyad { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "En Fazla 50 Karakter Girilebilir ! Lütfen Düzeltiniz!")]
         public string CariUnvan { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(20)]
+        [StringLength(20, ErrorMessage = "En Fazla 20 Karakter Girilebilir ! Lütfen Düzeltiniz!")]
         public string CariSehir { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "En Fazla 50 Karakter Girilebilir ! Lütfen Düzeltiniz!")]
         public string CariMail { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(50)]
+        [StringLength(50, ErrorMessage = "En Fazla 50 Karakter Girilebilir ! Lütfen Düzeltiniz!")]
         public string CariTelefon { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(250)]
+        [StringLength(250, ErrorMessage = "En Fazla 250 Karakter Girilebilir ! Lütfen Düzeltiniz!")]
         public string CariAdres { get; set; }
+
+        public bool Durum { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
 
     }
