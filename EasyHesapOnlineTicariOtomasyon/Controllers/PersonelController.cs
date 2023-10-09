16acb848060fc7b926365a7f3aa3c683bdd5234b
Tuncay Çakıroğlu <Tuncay.cakiroglu@icloud.com>
2023-10-09 23:49:50 +0300

    Personel Ekle Sil Güncelle İşlemleri Tamamlandı.

---
+++ EasyHesapOnlineTicariOtomasyon/Controllers/PersonelController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EasyHesapOnlineTicariOtomasyon.Models.Classes;
+
+namespace EasyHesapOnlineTicariOtomasyon.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        Context c=new Context();
+        public ActionResult Index()
+        {
+            var deger=c.Personels.ToList();
+            return View(deger);
+        }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAdi,
+                                               Value = x.Departmanid.ToString(),
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(Personel p)
+        {
+            c.Personels.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult PersonelGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Departmans.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmanAdi,
+                                               Value = x.Departmanid.ToString(),
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            var prs=c.Personels.Find(id);
+            return View("PersonelGetir",prs);
+        }
+        public ActionResult PersonelGuncelle(Personel p)
+        {
+            var prsn = c.Personels.Find(p.Personelid);
+            prsn.PersonelAd=p.PersonelAd;
+            prsn.PersonelSoyad=p.PersonelSoyad;
+            prsn.PersonelResim=p.PersonelResim;
+            prsn.Departmanid=p.Departmanid;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelSil(int id)
+        {
+            var personel = c.Personels.Find(id);
+            if (personel != null)
+            {
+                c.Personels.Remove(personel);
+                c.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
