207946f0e366d9973b341cde8f06d128d199834e
Tuncay Çakıroğlu <Tuncay.cakiroglu@icloud.com>
2023-10-16 15:09:29 +0300

    Cari Bilgiler Grafiği Tasarımı Yapıldı.

--- EasyHesapOnlineTicariOtomasyon/Controllers/istatistikController.cs
+++ EasyHesapOnlineTicariOtomasyon/Controllers/istatistikController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -37,22 +38,48 @@ namespace EasyHesapOnlineTicariOtomasyon.Controllers
             ViewBag.d11 = deger11;
             var deger12 = c.Uruns.GroupBy(x => x.MarkaAdi).OrderByDescending(y=>y.Count()).Select(z=> z.Key).FirstOrDefault();
             ViewBag.d12 = deger12;
-
             var deger13 =c.Uruns.Where(u=>u.Urunid==(c.SatisHarekets.GroupBy(x => x.Urunid).OrderByDescending(z => z.Count()).Select(y => y.Key).FirstOrDefault())).Select(k=> k.UrunAd).FirstOrDefault();
             ViewBag.d13 = deger13;
-
             var deger14=c.SatisHarekets.Sum(x=>x.ToplamTutar).ToString();
             ViewBag.d14 = deger14;
-            DateTime bugun=DateTime.Today;
+            DateTime bugun=DateTime.Now;
             var deger15 = c.SatisHarekets.Count(x => x.Tarih==bugun).ToString();
             ViewBag.d15 = deger15;
-            var deger16 = c.SatisHarekets.Where(x=>x.Tarih==bugun).Sum(y=>y.ToplamTutar).ToString();
-            ViewBag.d16 = deger16;
+            DateTime today = DateTime.Today;
+            decimal totalAmount = c.SatisHarekets
+                .Where(x => DbFunctions.TruncateTime(x.Tarih) == today)
+                .Sum(y => (decimal?)y.ToplamTutar) ?? 0;
+            string totalAmountString = totalAmount.ToString();
+            ViewBag.d16 = totalAmountString;
             return View();
         }
         public ActionResult SimpleTables()
         {
-            return View(); 
+            var sorgu = from x in c.Carilers
+                        group x by x.CariSehir into g
+                        select new GrupSinif
+                        {
+                            Sehir = g.Key,
+                            Sayi = g.Count()
+                        };
+            return View(sorgu.ToList()); 
+        }
+
+        public PartialViewResult Partial1()
+        {
+            var sorgu2 = from x in c.Personels
+                         group x by x.Departmanid into g
+                         select new SinifGrup2
+                         {
+                             Departman = g.Key,
+                             Sayi = g.Count()
+                         };
+            return PartialView(sorgu2.ToList());
+        }
+        public PartialViewResult Partial2() 
+        { 
+          var degerler=c.Carilers.ToList();
+          return PartialView(degerler);
         }
     }
 }
---
+++ EasyHesapOnlineTicariOtomasyon/Models/Classes/GrupSinif.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyHesapOnlineTicariOtomasyon.Models.Classes
+{
+    public class GrupSinif
+    {
+        public string Sehir { get; set; }
+        public int Sayi { get; set; }
+    }
+}
