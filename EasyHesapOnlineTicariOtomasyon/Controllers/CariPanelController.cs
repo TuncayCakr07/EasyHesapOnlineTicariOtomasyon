53bded59867a9e6bd84019c72c4fe6c43327ff3e
Tuncay Çakıroğlu <Tuncay.cakiroglu@icloud.com>
2023-10-17 23:09:34 +0300

    Sayfa Yönlendirme Link İşlemi Yapıldı.

--- EasyHesapOnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ EasyHesapOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.Mvc;
+using EasyHesapOnlineTicariOtomasyon.Models.Classes;
+using Context = EasyHesapOnlineTicariOtomasyon.Models.Classes.Context;
 
 namespace EasyHesapOnlineTicariOtomasyon.Controllers
 {
     public class CariPanelController : Controller
     {
+
+
         // GET: CariPanel
+        Context c = new Context();
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var mail = (string)Session["CariMail"];
+            var degerler=c.Carilers.FirstOrDefault(x=> x.CariMail==mail);
+            ViewBag.m = mail;
+            return View(degerler);
+        }
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CariMail"];
+            var id=c.Carilers.Where(x=> x.CariMail==mail.ToString()).Select(y=> y.Cariid).FirstOrDefault();
+            var degerler=c.SatisHarekets.Where(x=> x.Cariid==id).ToList();
+            return View(degerler);
         }
     }
 }
--- EasyHesapOnlineTicariOtomasyon/Controllers/LoginController.cs
+++ EasyHesapOnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace EasyHesapOnlineTicariOtomasyon.Controllers
 {
@@ -26,6 +27,27 @@ namespace EasyHesapOnlineTicariOtomasyon.Controllers
             c.Carilers.Add(p);
             c.SaveChanges();
             return PartialView();
-       }
+        }
+        [HttpGet]
+        public ActionResult CariLogin1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CariLogin1(Cariler p)
+        {
+            var bilgiler = c.Carilers.FirstOrDefault(x => x.CariMail == p.CariMail && x.CariSifre == p.CariSifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CariMail, false);
+                Session["CariMail"]=bilgiler.CariMail.ToString();
+                return RedirectToAction("Index","CariPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index","Login");
+            }
+            
+        }
     }
 }
